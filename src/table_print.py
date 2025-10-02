e10cd6d5d63f4f864f7127f2c2bdb3b1ee66a7d7
Jaibw <jai.unix@gmail.com>
2025-10-03 05:25:43 +0530

    Create table_print.py

---
+++ src/table_print.py
@@ -0,0 +1,28 @@
+"""
+Enter a number: 10
+
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+"""
+
+number1 = int(input("Enter a number: "))
+
+
+print(number1 * 1)
+print(number1 * 2)
+print(number1 * 3)
+print(number1 * 4)
+print(number1 * 5)
+print(number1 * 6)
+print(number1 * 7)
+print(number1 * 8)
+print(number1 * 9)
+print(number1 * 10)
