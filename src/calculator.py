4a6d3ae8fa6935bc4a538fbf31d141ec640a3871
Jaibw <jai.unix@gmail.com>
2025-10-03 04:40:17 +0530

    Create calculator.py

---
+++ src/calculator.py
@@ -0,0 +1,9 @@
+print("Calculator: Please enter two number")
+number1 = float(input("Number1: "))
+number2 = float(input("Number2: "))
+
+print("Addition: ", number1 + number2)
+print("Subtraction: ", number1 - number2)
+print("Multiplication: ", number1 * number2)
+print("Division: ", number1 / number2)
+print("Power: ", number1 ** number2)
