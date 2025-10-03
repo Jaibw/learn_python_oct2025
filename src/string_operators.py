b9a56857130f69c6ecdbce81f26f16a9f4510333
Jaibw <jai.unix@gmail.com>
2025-10-04 05:26:47 +0530

    Update string_operators.py

--- src/string_operators.py
+++ src/string_operators.py
@@ -5,3 +5,18 @@ string02 = input("String2: ")
 print("="*30)
 print("Concat: ", string01 + string02)
 print("="*30)
+
+string01 = input("Enter a string: ")
+
+# string01 = string01.capitalize()
+# print(string01)
+
+# string01 = string01.upper()
+# print(string01)
+
+print(string01.split())
+print(string01.strip())
+
+print(string01.isnumeric())
+print(string01.isalpha())
+print(string01.isupper())
