6bbdfc4e49a0d4cf3c219c05d2622c0db33127aa
Jaibw <jai.unix@gmail.com>
2025-10-04 04:59:44 +0530

    Create vote.py

---
+++ src/vote.py
@@ -0,0 +1,9 @@
+username = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Congratulations! You are old enough.")
+    print("You can vote!")
+else:
+    print("Sorry, you are not old enough.")
+    print("You can not vote!")
