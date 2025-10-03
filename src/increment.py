197560706291ee48272fa55d801bc37f0143998e
Jaibw <jai.unix@gmail.com>
2025-10-04 04:36:54 +0530

    Create increment.py

---
+++ src/increment.py
@@ -0,0 +1,14 @@
+number1 = 15
+
+## Increment
+print(number1)
+#number1 = number1 + 1
+number1 += 1
+print(number1)
+
+## Multiply
+number2 = 10
+print(number2)
+# number2 = number2 * 2 
+number2 *= 2
+print(number2)
