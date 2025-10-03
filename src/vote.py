7df910d0c9a43bcc67105116b523971279ac8559
Jaibw <jai.unix@gmail.com>
2025-10-04 05:02:20 +0530

    Update vote.py

--- src/vote.py
+++ src/vote.py
@@ -1,6 +1,9 @@
 username = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
+if username == "":
+    print("You didn't enter a name") 
+    
 if age >= 18:
     print("Congratulations! You are old enough.")
     print("You can vote!")
