f72c4a31ec9b30687c3825ce6e04255a226c458b
Jaibw <jai.unix@gmail.com>
2025-10-22 04:42:37 +0530

    Add tuple example with print statements

---
+++ src/tuple01.py
@@ -0,0 +1,18 @@
+tuple01 = (1,2,3,4,5)
+tuple02 = (1,)
+
+for item in tuple01:
+    print(item)
+print("+"*30)
+print(tuple01[0])
+print("+"*30)
+#tuple01[0] = 2
+
+list01 = [1,2,3,4,5]
+list01[0] = 6
+
+## We don't change value
+apply_config = (
+    "db01.server.url",
+    "a[~6LuuQYnP/t~X(yLL!o,S@fa5A$G",
+)
