1b7bc93bea8efdcf9699aeaf936a0b1bea2e370c
Jaibw <jai.unix@gmail.com>
2025-10-22 05:21:33 +0530

    Create sets_demo01.py

---
+++ src/sets_demo01.py
@@ -0,0 +1,18 @@
+students = {
+    "John",
+    "Mark",
+    "John"
+}
+
+print(type(students))
+print(students)
+
+students01 = [
+    "John",
+    "Mark",
+    "John"
+]
+print(students01)
+
+students03 = set(students01)
+print(students03)
