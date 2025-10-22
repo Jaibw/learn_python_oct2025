eddeaca5726657bbaef54cbad88f08d6d02ad66b
Jaibw <jai.unix@gmail.com>
2025-10-23 04:39:51 +0530

    Create dict_demo.py

---
+++ src/dict_demo.py
@@ -0,0 +1,15 @@
+student = {
+    'firstName': 'John',
+    'lastName': 'Doe',
+    'email': 'aa@aa.com',
+    'age': 22,
+    'mobile': 99999999
+}
+
+students = []
+students.append(student)
+
+print(student)
+print(student['firstName'])
+print(student['lastName'])
+print(student['email'])
