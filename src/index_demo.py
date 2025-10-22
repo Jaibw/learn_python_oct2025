533089b1fcbbb18d55999299c08cdcebe5ffda1f
Jaibw <jai.unix@gmail.com>
2025-10-23 05:00:07 +0530

    Update index_demo.py

--- src/index_demo.py
+++ src/index_demo.py
@@ -1,8 +1,11 @@
 str1 = 'Hello World'
 for x in range(len(str1)):
     print(str1[x], end='_')
-    
 print()
+print(str1[:5])
+print(str1[6:])
+print(str1[6:8])
+
 print('='*20)
 
 for x in range(-1, (-1 * len(str1)) -1, -1):
