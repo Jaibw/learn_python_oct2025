51a91746d6b63da0974976c122913ffd59b22f9b
Jaibw <jai.unix@gmail.com>
2025-10-25 04:53:44 +0530

    Implement header function for formatted output
    Added a header function to format output messages.

--- src/titanic.py
+++ src/titanic.py
@@ -1,6 +1,20 @@
 import pandas as pd
+def header(message):
+    print('=' * 100)
+    print(message)
+    print('=' * 100)
 
 ## load data
 df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
-print(df.head())
+header("Print few records")
+print(df.head(2))
 
+## understand data
+header("Print Basic Information")
+df.info()
+header("Shape of my data")
+print(df.shape)
+header("Column names")
+print(df.columns)
+header("statistics of my data")
+print(df.describe())
