b8df56ff31017b7e27bbed52725d77dfd6f67ea0
Jaibw <jai.unix@gmail.com>
2025-10-26 05:04:56 +0530

    Create project.py

---
+++ src/project.py
@@ -0,0 +1,9 @@
+"""
+load data from - https://raw.githubusercontent.com/datasets/population/master/data/population.csv
+
+> load data
+> understand data
+> clean it if needed
+> save it in the database (SQL/Snowflake/CSV)
+
+"""
